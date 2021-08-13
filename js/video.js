dffb15198c04705845ad170759eac50f989a48e1
Lmho546 <Lmho546P@outlook.com>
2021-08-13 13:12:46 -0300

    Ajustes Y Modificado Video

--- js/video.js
+++ js/video.js
@@ -1,67 +1,84 @@
-const backgroundVideo = document.getElementsByClassName ("backgroundVideo")[0];
-const videoAll = document.getElementsByClassName ("videoAll")[0];
-const videoControls = document.getElementsByClassName ("video")[0];
-const video = document.getElementsByClassName ("video")[0].getElementsByTagName("source")[0];
+const backgroundVideoHome = document.getElementsByClassName ("backgroundVideoHome")[0];
+const videoAllHome = document.getElementsByClassName ("videoAllHome")[0];
+const videoControlsHome = document.getElementsByClassName ("videoHome")[0];
+const videoHome = document.getElementsByClassName ("videoHome")[0].getElementsByTagName("source")[0];
 
-const buttonClose = document.getElementById ("buttonClose");
+const backgroundVideoConsultancies = document.getElementsByClassName ("backgroundVideoConsultancies")[0];
+const videoAllConsultancies = document.getElementsByClassName ("videoAllConsultancies")[0];
+const videoControlsConsultancies = document.getElementsByClassName ("videoConsultancies")[0];
+const videoConsultancies = document.getElementsByClassName ("videoConsultancies")[0].getElementsByTagName("source")[0];
 
-//const videoList = document.getElementById ("videos").getElementsByClassName ("item");
+const buttonCloseHome = document.getElementById ("buttonCloseHome");
+const buttonCloseConsultancies = document.getElementById ("buttonCloseConsultancies");
 
-//videoList[0].addEventListener('click', () => {WatchVideo("2");});
+buttonCloseHome.onclick = CloseVideo;
+buttonCloseConsultancies.onclick = CloseVideo;
 
-buttonClose.onclick = CloseVideo;
-
-function WatchVideo(videoNumber) {
+function WatchVideo(videoNumber, pageNumber) {
 
     switch(videoNumber) {
 
         case "1":
-            video.setAttribute("src", "vid/vid-1.mp4");
+            videoHome.setAttribute("src", "vid/vid-1.mp4");
+            videoConsultancies.setAttribute("src", "vid/vid-1.mp4");
             break;
 
         case "2":
-            video.setAttribute("src", "vid/vid-2.mp4");
+            videoConsultancies.setAttribute("src", "vid/vid-2.mp4");
             break;
 
         case "3":
-            video.setAttribute("src", "vid/vid-3.mp4");
+            videoHome.setAttribute("src", "vid/vid-3.mp4");
+            videoConsultancies.setAttribute("src", "vid/vid-3.mp4");
             break;
     
         case "4":
-            video.setAttribute("src", "vid/vid-4.mp4");
+            videoConsultancies.setAttribute("src", "vid/vid-4.mp4");
             break;  
 
         case "5":
-            video.setAttribute("src", "vid/vid-5.mp4");
+            videoConsultancies.setAttribute("src", "vid/vid-5.mp4");
             break;
     
         case "6":
-            video.setAttribute("src", "vid/vid-6.mp4");
+            videoConsultancies.setAttribute("src", "vid/vid-6.mp4");
             break;
     
         case "7":
-            video.setAttribute("src", "vid/vid-7.mp4");
+            videoConsultancies.setAttribute("src", "vid/vid-7.mp4");
             break;
         
         case "8":
-            video.setAttribute("src", "vid/vid-8.mp4");
+            videoConsultancies.setAttribute("src", "vid/vid-8.mp4");
             break;  
 
         default:
-            video.setAttribute("src", "vid/vid-1.mp4");
+            videoConsultancies.setAttribute("src", "vid/vid-1.mp4");
             break;       
     };
-    
-    videoControls.load();
-    backgroundVideo.classList.add("activeVideo");
-    videoAll.classList.add("activeVideo");
-    
+
+    if(pageNumber == "1") {
+        videoControlsHome.load();
+        backgroundVideoHome.classList.add("activeVideo");
+        videoAllHome.classList.add("activeVideo");
+    }
+
+    if(pageNumber == "2") {
+        videoControlsConsultancies.load();
+        backgroundVideoConsultancies.classList.add("activeVideo");
+        videoAllConsultancies.classList.add("activeVideo");
+    }
+
 };
 
 function CloseVideo () {
 
-    backgroundVideo.classList.remove("activeVideo");
-    videoAll.classList.remove("activeVideo");
-    videoControls.pause();
+    backgroundVideoHome.classList.remove("activeVideo");
+    videoAllHome.classList.remove("activeVideo");
+    videoControlsHome.pause();
+
+    backgroundVideoConsultancies.classList.remove("activeVideo");
+    videoAllConsultancies.classList.remove("activeVideo");
+    videoControlsConsultancies.pause();
 
 };
